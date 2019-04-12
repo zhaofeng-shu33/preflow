981b783160aaaa0d8ee5c752e61616c2a41f381e
zhaofeng-shu33 <616545598@qq.com>
2019-04-12 21:27:22 +0800

    demo of how to use concept to modify elevator type

--- concept_test.cpp
+++ concept_test.cpp
@@ -1,8 +1,17 @@
 // test graph concept
 #include <lemon/concepts/digraph.h>
-#include <preflow.h>
+#include <lemon/preflow.h>
+#include <lemon/elevator.h>
 using namespace lemon;
 int main(){
-   Preflow<Digraph, Digraph::ArcMap> pf;
-
+   typedef concepts::Digraph Digraph;
+   typedef double T;
+   typedef Digraph::ArcMap<T> ArcMap;
+   Digraph g;
+   ArcMap aM(g);
+   Preflow<Digraph, ArcMap> pf(g, aM, INVALID, INVALID);
+   // the default traint for Preflow is Elevator,
+   // suppose we would like to use LinkedElevator, we can do it by
+   typedef LinkedElevator<Digraph, Digraph::Node> LinkedElevator;
+   Preflow<Digraph, ArcMap>::SetElevator<LinkedElevator>::Create pf2(g, aM, INVALID, INVALID);
 }
