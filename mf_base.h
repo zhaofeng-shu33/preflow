b50e3a2f844776ca739bdb655669404b04c3a622
zhaofeng-shu33 <616545598@qq.com>
2019-04-12 21:52:13 +0800

    define some type

--- mf_base.h
+++ mf_base.h
@@ -1,10 +1,22 @@
-#include <lemon/list_graph.h>
+#include <lemon/preflow.h>
 namespace experiment{
-    class Preflow{
-        typedef lemon::ListDigraph Digraph;
-        typedef lemon::ListDigraph::ArcMap<double> ArcMap;
-        typedef ArcMap CapacityMap;
-        typedef Digraph::Node Node;
+    template <typename GR,
+              typename CAP = typename GR::template ArcMap<int>,
+              typename TR = lemon::PreflowDefaultTraits<GR, CAP> >
+    class Preflow_Relabel{
+        private:
+
+            TEMPLATE_DIGRAPH_TYPEDEFS(Digraph);        
+        
+        public:
+            // for linked list we use std::list<Node>
+            typedef TR Traits;
+            typedef typename Traits::Digraph Digraph;
+            typedef typename Traits::CapacityMap CapacityMap;
+            typedef typename Traits::Value Value;
+            typedef typename Traits::FlowMap FlowMap;
+            typedef std::list<Node> Elevator;
+            typedef typename Traits::Tolerance Tolerance;        
     };
 
 }
