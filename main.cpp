94eefcf1a2557f7c00f57dde65bf95bbd719b396
zhaofeng-shu33 <616545598@qq.com>
2019-04-13 08:55:42 +0800

    use cmake to build

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
+#include <gtest/gtest.h>
 #include <lemon/concepts/digraph.h>
 #include "mf_base.h"
 using namespace lemon;
-int main(){
+TEST(Preflow_Relabel, Constructor) {
     typedef concepts::Digraph Digraph;
     typedef double T;
     typedef Digraph::ArcMap<T> ArcMap;
     Digraph g;
     ArcMap aM(g);
     Preflow_Relabel<Digraph, ArcMap> pf(g, aM, INVALID, INVALID);
+}
+
+TEST(RelabelElevator, Constructor) {
+    typedef concepts::Digraph Digraph;
+    typedef Digraph::Node Item;
+    Digraph g;
+    RelabelElevator<Digraph, Item> re(g, 0);
 }
--- mf_base.h
+++ mf_base.h
@@ -1,5 +1,70 @@
 #include <lemon/preflow.h>
 namespace lemon{
+    template<class GR, class Item>
+    class RelabelElevator{
+        //relabel to front elevator
+        
+    public:
+        typedef Item Key;
+        typedef int Value;
+        
+    private:
+        typedef typename ItemSetTraits<GR, Item>::
+        template Map<int>::Type IntMap;
+        typedef typename ItemSetTraits<GR, Item>::
+        template Map<bool>::Type BoolMap;
+        
+        const GR &_graph;
+        int _max_level;
+        IntMap _level;
+        BoolMap _active;
+        std::list<Item> relabel_list;
+    public:
+        RelabelElevator(const GR& graph, int max_level)
+        : _graph(graph), _max_level(max_level),
+          _level(graph), _active(graph) {}
+          
+        void activate(Item i) {
+            _active[i] = true;
+        }
+        
+        void deactivate(Item i) {
+            _active[i] = false;
+        }
+        
+        bool active(Item i) const { return _active[i]; }
+        
+        int operator[](Item i) const { return _level[i]; }
+        
+        // move the Item to the front of relabel_list
+        void moveToFront(std::list<Item>::iterator item_it) {
+            Item item = *item_it;
+            relabel_list.erase(item_it)
+            relabel_list.push_front(item);
+        }
+    
+    private:
+        
+        int _init_level;
+        
+        void initStart() {
+            _init_level = 0;
+            for(typename ItemSetTraits<GR, Item>::ItemIt i(_graph);
+                i != INVALID; ++i) {
+                _level[i] = _max_level;
+                _active[i] = false;
+            }
+        }
+        void initAddItem(Item i) {
+            _level[i] = _init_level;
+            relabel_list.push_back(i);
+        }
+        void initNewLevel() {
+            ++_init_level;
+        }
+        void initFinish() {            
+        }
+    }
     template <typename GR,
               typename CAP = typename GR::template ArcMap<int>,
               typename TR = PreflowDefaultTraits<GR, CAP> >
