dd6b16cbefbae27b02c0e980220c113e9c13e0c4
zhaofeng-shu33 <616545598@qq.com>
2019-05-09 00:09:11 +0800

    add link lib for unix

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <boost/program_options.hpp>
 #include <lemon/lgf_reader.h>
 #include <lemon/list_graph.h>
@@ -10,7 +11,8 @@ int main(int argc, const char *argv[]){
 	desc.add_options()
 		("help,h", "Show this help screen")
 		("filename", boost::program_options::value<std::string>(), "lgf file name")
-		("use_original", boost::program_options::value<bool>()->implicit_value(true)->default_value(false), "whether to use the original highest label implementation");
+		("use_original", boost::program_options::value<bool>()->implicit_value(true)->default_value(false), "whether to use the original highest label implementation")
+		("print_cut", boost::program_options::value<bool>()->implicit_value(true)->default_value(false), "whether to print the min cut set of source side");
 
 	boost::program_options::command_line_parser parser{ argc, argv };
 	parser.options(desc);
@@ -30,8 +32,10 @@ int main(int argc, const char *argv[]){
 	}
 	std::string filename;
 	bool use_original_algorithm;
+	bool print_cut;
 	try{
 		use_original_algorithm = vm["use_original"].as<bool>();
+		print_cut = vm["print_cut"].as<bool>();
 		filename = vm["filename"].as<std::string>();
 	}
 	catch (const boost::program_options::error & ex) {
@@ -43,6 +47,7 @@ int main(int argc, const char *argv[]){
 	typedef double T;
 	typedef Digraph::ArcMap<T> ArcMap;
 	typedef Digraph::Node Node;
+	typedef Digraph::NodeIt NodeIt;
 
 	Digraph digraph;
 	ArcMap cap(digraph);
@@ -54,18 +59,33 @@ int main(int argc, const char *argv[]){
 		.run();
 	double max_flow_value;
 	std::string method_name;
+	std::stringstream cut_set;
+	cut_set << '{';
 	if (use_original_algorithm) {
 		lemon::Preflow<Digraph, ArcMap> alg(digraph, cap, src, trg);
 		method_name = "highest label";
 		alg.run();
 		max_flow_value = alg.flowValue();
+		for (NodeIt n(digraph); n != lemon::INVALID; ++n) {
+			if (alg.minCut(n))
+				cut_set << digraph.id(n) << ',';
+		}
 	}
 	else {
 		lemon::Preflow_Relabel<Digraph, ArcMap> alg(digraph, cap, src, trg);
 		method_name = "relabel";
 		alg.run();
 		max_flow_value = alg.flowValue();
+		for (NodeIt n(digraph); n != lemon::INVALID; ++n) {
+			if (alg.minCut(n))
+				cut_set << digraph.id(n) << ',';
+		}
 	}
 	std::cout << "using " << method_name  <<", max flow value : " << max_flow_value << '\n';
+	if (print_cut) {
+		std::string cut_set_string = cut_set.str();
+		cut_set_string[cut_set_string.length() - 1] = '}';
+		std::cout << "min cut set of source side: " << cut_set_string << '\n';
+	}
 	return 0;
 }
