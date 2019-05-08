07431e6de1d5f9615e1c04b615102c5e4288e70b
zhaofeng-shu33 <616545598@qq.com>
2019-05-08 23:41:47 +0800

    abandon positional argument.

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Implementation of preflow algorithm with a different
 
 [![Travis](https://travis-ci.com/zhaofeng-shu33/preflow.svg?branch=master)](
     https://travis-ci.com/zhaofeng-shu33/preflow)
+    
+## How to use
+After successful compilation and link, you get an executable program called `lgf_compute`. You should use an LEMON graph file (digraph)
+as input to this program, and the program print out the caculated result to the terminal. 
+For example,  using provided test graph file, type `./lgf_compute --filename test.lgf` to finish the computation.
--- main.cpp
+++ main.cpp
@@ -9,19 +9,22 @@ int main(int argc, const char *argv[]){
     boost::program_options::options_description desc;
 	desc.add_options()
 		("help,h", "Show this help screen")
-		("use_original", boost::program_options::value<bool>()->implicit_value(true)->default_value(false), "whether to use the original test");
+		("filename", boost::program_options::value<std::string>(), "lgf file name")
+		("use_original", boost::program_options::value<bool>()->implicit_value(true)->default_value(false), "whether to use the original highest label implementation");
 
-    boost::program_options::positional_options_description pos_desc;
-	pos_desc.add("filename", 1);
 	boost::program_options::command_line_parser parser{ argc, argv };
-	parser.options(desc).positional(pos_desc);
-	boost::program_options::parsed_options parsed_options = parser.run();
-
+	parser.options(desc);
 	boost::program_options::variables_map vm;
-	boost::program_options::store(parsed_options, vm);
-	boost::program_options::notify(vm);
 
-	if (vm.count("help") || !vm.count("finename")) {
+	try{
+		boost::program_options::parsed_options parsed_options = parser.run();
+		boost::program_options::store(parsed_options, vm);
+		boost::program_options::notify(vm);
+	}
+	catch (const boost::program_options::error & ex) {
+		std::cerr << ex.what() << '\n';
+	}
+	if (vm.count("help") || !vm.count("filename")) {
 		std::cout << desc << '\n';
 		return 0;
 	}
@@ -50,16 +53,19 @@ int main(int argc, const char *argv[]){
 		.node("target", trg)
 		.run();
 	double max_flow_value;
+	std::string method_name;
 	if (use_original_algorithm) {
 		lemon::Preflow<Digraph, ArcMap> alg(digraph, cap, src, trg);
+		method_name = "highest label";
 		alg.run();
 		max_flow_value = alg.flowValue();
 	}
 	else {
 		lemon::Preflow_Relabel<Digraph, ArcMap> alg(digraph, cap, src, trg);
+		method_name = "relabel";
 		alg.run();
 		max_flow_value = alg.flowValue();
 	}
-	std::cout << "max flow value : " << max_flow_value << '\n';
+	std::cout << "using " << method_name  <<", max flow value : " << max_flow_value << '\n';
 	return 0;
 }
