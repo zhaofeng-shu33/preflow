f1daa52e785932428cdd8732f0bd63f7f1f9cb4d
zhaofeng-shu33 <616545598@qq.com>
2019-05-28 19:16:14 +0800

    add new util

---
+++ utility/gml2lgf.py
@@ -0,0 +1,37 @@
+'''This program converts gml format to LGF graph format(LEMON C++)
+currently only digraph convertion is supported.
+'''
+
+import argparse
+import networkx as nx
+import pdb
+def convert(filename):
+    digraph = toNetworkX(filename)
+    Ls = ['@nodes', 'label']
+    for i in digraph.nodes:
+        Ls.append(i)
+    Ls.append('@arcs')
+    Ls.append('\t\tlabel\tcapacity')
+    edge_cnt = 0
+    for e in digraph.edges(data=True):
+        i, j, dic = e
+        w = 1
+        if(dic.get('weight')):
+            w = dic['weight']
+        Ls.append('\t'.join([i, j, str(edge_cnt), str(w)]))
+    with open(filename.replace('gml','lgf'),'w') as f:
+        f.write('\n'.join(Ls))
+
+    
+def toNetworkX(filename):
+    digraph = nx.gml.read_gml(filename)
+    return digraph
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('gml to lgf')
+    parser.add_argument('filename', help='GML file to be converted')
+    parser.add_argument('--debug', help='enter debug mode', default=False, type=bool, nargs='?', const=True)
+    args = parser.parse_args()
+    if(args.debug):
+        pdb.set_trace()
+    convert(args.filename)
