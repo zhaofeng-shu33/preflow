ee495b41de9dcb513cf637a3bc205e7834dff05d
zhaofeng-shu33 <616545598@qq.com>
2019-08-10 10:03:00 +0800

    make the converter handle str

--- utility/lgf2gv.py
+++ utility/lgf2gv.py
@@ -1,45 +1,59 @@
 '''This program converts LGF graph format(LEMON C++) to gv format (graphviz)
 currently only digraph convertion is supported.
 '''
-
 import argparse
 import networkx as nx
 import pdb
-def toNetworkX(filename):
+
+def get_readline_generator(str):
+    str_inner = str
+    if(str[-1] != '\n'):
+        str_inner += '\n'
+    str_inner += '@attributes'
+    for i in str_inner.split('\n'):
+        yield i
+        
+def toNetworkX(contents):
     dot = nx.DiGraph()
-    with open(filename) as f:
-        st = f.readline()
-        st = f.readline()
-        while(True):
-            if(st.find('@arcs')>=0):
-                break
-            if(st.find('label')>=0):
-                st = f.readline()
-                continue
-            node_label = st.strip()
-            dot.add_node(node_label)
-            st = f.readline()
-        st = f.readline()
-        while(True):
-            if(st.find('@attributes')>=0):
-                break
-            if(st.find('label')>=0):
-                st = f.readline()
-                continue
-            s,t,_,c = st.strip().split('\t')
-            dot.add_edge(s,t,label=str(c))
-            st = f.readline()
-        if(st.find('source')>=0):
-            _,s_id = st.strip().split(' ')
-            dot.nodes[s_id]['label'] = 's'
-        st = f.readline()
-        if(st.find('target')>=0):
+    f = get_readline_generator(contents)
+    f.__next__()
+    st = f.__next__()
+    while(True):
+        if(st.find('@arcs') >= 0):
+            break
+        if(st.find('label') >= 0):
+            st = f.__next__()
+            continue
+        node_label = st.strip()
+        dot.add_node(node_label)
+        st = f.__next__()
+    st = f.__next__()
+    while(True):
+        if(st.find('@attributes') >= 0):
+            break
+        if(st.find('label') >= 0):
+            st = f.__next__()
+            continue
+        s,t,_,c = st.strip().split('\t')
+        dot.add_edge(s, t, label=str(c))
+        st = f.__next__()
+    if(st.find('source') >= 0):
+        _,s_id = st.strip().split(' ')
+        dot.nodes[s_id]['label'] = 's'    
+        st = f.__next__()
+        if(st.find('target') >= 0):
             _,t_id = st.strip().split(' ')
             dot.nodes[t_id]['label'] = 't'
     return dot
+
+def get_contents(filename):
+    with open(filename) as f:
+        st = f.read()
+    return st
     
 def convert(filename):
-    dot = toNetworkX(filename)
+    contents = get_contents(filename)    
+    dot = toNetworkX(contents)
     nx.drawing.nx_pydot.write_dot(dot, filename.replace('lgf','gv'))
     
 if __name__ == '__main__':
